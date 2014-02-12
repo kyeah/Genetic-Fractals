8098bd005bab68bec603e7a48c6bef737510a972
Kevin Yeh <kevinyeah@utexas.edu>
2014-02-12 13:12:20 -0600

    Added AntTweakBar

---
+++ fractalEditor.h
@@ -0,0 +1,13 @@
+#ifndef __FRACTALEDITOR_H__
+#define __FRACTALEDITOR_H__
+#include "common.h"
+#include "fractal.h"
+
+int createFractalEditorWindow(string _name);
+void feRepaint();
+void feRegisterCallbacks();
+void feResize(int w, int h);
+void feReshape(int w, int h);
+void feResize();
+
+void feMouseButton(int button, int state, int x, int y);
